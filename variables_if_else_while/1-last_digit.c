0dea7c0bc0d3094e3452e2f5bb87eeda48c849bf
ismael <ismaelbsd.pro@gmail.com>
2025-10-20 15:14:43 +0000

    variable

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -10,26 +10,25 @@
 int main(void)
 {
 	int n;
-	int tast_digit;
+	int last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
 
-	printf("le dernier chiffre de %d est %d", n, tast_digit);
+	printf("le dernier chiffre de %d est %d\n", n, last_digit);
 
 	if (last_digit > 5)
 	{
-		printf("et est supprieur a 5\n")
+		printf("et est supprieur a 5\n");
 	}
 	else if (last_digit == 0)
 	{
-		printf("et est egal a 0\n")
+		printf("et est egal a 0\n");
 	}
 	else
 	{
 		printf("et est inferieur a 6 et different de 0\n");
 	}
-
 	return (0);
 }
