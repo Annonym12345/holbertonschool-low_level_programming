558093d9303519eb436b94b40decedd7e8b2dc19
ismael <ismaelbsd.pro@gmail.com>
2025-10-20 00:39:55 +0000

    hello

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
-// Betty on vas pas etre amis nous deux si ca continue comme ca hein mdr
 #include <stdio.h>
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
+// Betty on vas pas etre amis nous deux si ca continue comme ca hein mdr
