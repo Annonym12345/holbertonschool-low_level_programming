1d87dba340d9a32ab951437d62c3bfc173cc5fa0
ismael <ismaelbsd.pro@gmail.com>
2025-10-21 15:39:12 +0000

    variable

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-        char a;
+	char a;
 
-        a = 97;
-        while (a >= 'a' && a <= 'z')
-        {
+	a = 97;
+	while (a >= 'a' && a <= 'z')
+	{
 		if (a != 'q' && a != 'e')
 		{
 			putchar(a);
 		}
 		a++;
-        }
-        putchar(10);
-        return (0);
+	}
+	putchar(10);
+	return (0);
 }
