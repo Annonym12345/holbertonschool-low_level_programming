4537144c9e355e3da9d6e51054e343788ec78409
ismael <ismaelbsd.pro@gmail.com>
2025-10-22 13:02:27 +0000

    variable

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,18 +7,17 @@
  */
 int main(void)
 {
-        int i;
-
-        i = 0;
-        for (i = 0; i <= 9; i++)
-        {
+	int i;
+	
+	i = 0;
+	for (i = 0; i <= 9; i++)
+	{
 		putchar((i % 10) + '0');
 		if (i == 9)
 			continue;
-		
 		putchar(',');
 		putchar(' ');
 	}
 	putchar(10);
-        return (0);
+	return (0);
 }
