64e26c08afea8af734bd6d0b77dc5bf8973fa524
ismael <ismaelbsd.pro@gmail.com>
2025-10-22 10:01:25 +0000

    variable

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -7,21 +7,21 @@
  */
 int main(void)
 {
-        char a;
-        char c;
+	char a;
+	char c;
 
-        a = 48;
-        c = 97;
-        while (a >= '0' && a <= '9')
-        {
-                putchar(a);
-                a++;
-        }
-        while (c >= 'a' && c <= 'f')
-        {
-                putchar(c);
-                c++;
-        }
-        putchar(10);
-        return (0);
+	a = 48;
+	c = 97;
+	while (a >= '0' && a <= '9')
+	{
+		putchar(a);
+		a++;
+	}
+	while (c >= 'a' && c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar(10);
+	return (0);
 }
