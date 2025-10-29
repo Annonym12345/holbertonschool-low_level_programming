3af1307fa05463635e0264d978fa9677d94e1e97
ismael <ismaelbsd.pro@gmail.com>
2025-10-29 09:31:40 +0000

    loops

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,13 +1,11 @@
-# Git Intro Project
-
-//#include <....h>
+#include <stdio.h>
 
 /**
- ** main - Prints the numbers from 1-100, but for multiples of three,
- **        Fizz is printed instead of the number, for multiples of five,
- **        Buzz, and for multiples of both three and five, FizzBuzz.
+ * main - Prints the numbers from 1-100, but for multiples of three,
+ *        Fizz is printed instead of the number, for multiples of five,
+ *        Buzz, and for multiples of both three and five, FizzBuzz.
  *
- ** Return: Always 0.
+ * Return: Always 0.
  */
 int main(void)
 {
