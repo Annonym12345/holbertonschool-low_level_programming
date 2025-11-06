b99da8907e5dae80886eb4d9a732f6366f18074e
ismael <ismaelbsd.pro@gmail.com>
2025-11-06 14:10:38 +0000

    argc

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - Prints the program name, followed by a new line.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
+ * main - prints the program name followed by a new line
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: Always o.
+ * Return: Always 0
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
